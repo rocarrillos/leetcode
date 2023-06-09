ec7019f74bd35dd949d6d27c5d1ecb6f80535d92
Rodrigo Carrillo-Sanchez <rocarrillos@hotmail.com>
2023-06-08 22:17:16 -0400

    another day another challenge

---
+++ find-smallest-letter-greater-than-target.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        index_to_chop = 0
+        for i in range(len(letters)):
+            if letters[i] <= target:
+                index_to_chop = i + 1
+        survivors = letters[index_to_chop:]
+        if len(survivors):
+            return survivors[0]
+        return letters[0]
