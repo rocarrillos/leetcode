dc6281c1b7cb9e2d9265d1af8674c4a8a4921f1c
Rodrigo Carrillo-Sanchez <rocarrillos@hotmail.com>
2023-06-09 22:32:58 -0400

    candies v2

--- kids-with-the-greatest-number-of-candies.py
+++ kids-with-the-greatest-number-of-candies.py
@@ -1,4 +1,6 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         current_greatest = max(candies)
-        return list(map(lambda x: (x + extraCandies) >= current_greatest, candies))
+        # return list(map(lambda x: (x + extraCandies) >= current_greatest, candies))
+        # apparently list comprehension is faster than lambda
+        return [x + extraCandies >= current_greatest for x in candies]
