1bf7a12ca406bd95c0c8a934d1d713dde8a9f225
Rodrigo Carrillo-Sanchez <rocarrillos@hotmail.com>
2023-06-11 21:08:11 -0400

    did june 11 on time

---
+++ summary-ranges.py
@@ -0,0 +1,28 @@
+class Solution:
+    # not super fast but same idea as the fast solutions
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        if len(nums) == 0:
+            return []
+        ranges = []
+        lower = None
+        upper = None
+        for i in range(len(nums)):
+            if i == 0:
+                # begin
+                lower = nums[i]
+                upper = nums[i]
+            else:
+                if nums[i] == upper + 1:
+                    upper = nums[i]
+                else:
+                    if lower == upper:
+                        ranges.append(str(lower))
+                    else:
+                        ranges.append(str(lower) + "->" + str(upper))
+                    lower = nums[i]
+                    upper = nums[i]
+        if lower == upper:
+            ranges.append(str(lower))
+        else:
+            ranges.append(str(lower) + "->" + str(upper))
+        return ranges
