fdff60b952695bf110cff4cf4355a9a99d3faa12
Rodrigo Carrillo-Sanchez <rocarrillos@hotmail.com>
2023-06-13 21:11:53 -0400

    cleanup

--- minimum-absolute-difference-in-bst.py
+++ minimum-absolute-difference-in-bst.py
@@ -1,11 +1,12 @@
 # This is the same as minimum-distance-between-bst-nodes
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
 def traverseTree(node: TreeNode, values: set):
     values.add(node.val)
     if node.right is not None:
